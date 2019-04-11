7b97413e7a6d5754e7033124f4afdeaea7c44797
tshmak <timothy@fano.ai>
2019-04-11 15:56:31 +0800

    Adds jupyter option for disabling autosave

---
+++ jupyter/custom.js
@@ -0,0 +1,15 @@
+define([
+    'base/js/namespace',
+    'base/js/events'
+    ],
+    function(IPython, events) {
+      events.on("notebook_loaded.Notebook",
+          function () {
+                                 IPython.notebook.set_autosave_interval(180000);
+                                //in milliseconds
+                   }
+        );
+      //may include additional events.on() statements
+   }
+);
+
