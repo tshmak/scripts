9e72dc08bb343d72a4e65f82085537b548690a70
tshmak <timothy@fano.ai>
2021-01-12 15:09:01 +0800

    Improves load_my_preferences.sh

--- load_my_preferences.sh
+++ load_my_preferences.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-_conda_exec=$(which conda)
-if [ ! $_conda_exec ] 
+_conda_dir=$(ls -d $HOME/*conda3 2>/dev/null) 
+if [ "$?" != "0" ] 
 then
     echo "conda not found"
     echo "Consider: wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-    echo "Otherwise: . \$conda_path/etc/profile.d/conda.sh"  
 else
-    echo "conda found: $_conda_exec"
+    echo "conda found: $_conda_dir"
+    source $_conda_dir/etc/profile.d/conda.sh
 fi
 echo
 
